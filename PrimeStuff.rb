dac48e4f9e2769034937f1a8d9337261f5dc8cd6
Jonathan Potter <jonathan.potter.business@gmail.com>
2013-09-16 18:49:40 -0400

    initial commit

---
+++ PrimeStuff.rb
@@ -0,0 +1,23 @@
+
+
+module MathStuff
+
+  def primes_below_limit(limit)
+    primes = Array.new(limit).map.with_index { |t,i| i }
+
+    2.upto(limit) do |index|
+      number = index
+      begin
+        number += index
+        primes[number] = nil
+      end until number > limit
+    end
+
+    primes.select { |element| !element.nil? }
+  end
+
+end
+
+if __FILE__ == $0
+  p primes_below_limit(1000)
+end
