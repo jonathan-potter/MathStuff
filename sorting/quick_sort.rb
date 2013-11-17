43210c0efc6801dbfdf8ec07bfddc167be8d15fe
Jonathan Potter <jonathan.potter.business@gmail.com>
2013-11-16 22:59:28 -0500

    refactors quicksort for greater readability

--- sorting/quick_sort.rb
+++ sorting/quick_sort.rb
@@ -2,34 +2,28 @@
 
 def quicksort(array)
   tree = nil
-
-  array.each do |element|
-    tree = build_quicksort_tree( element, tree )
-  end
-
+  array.each { |element| tree = build_quicksort_tree(element, tree) }
   deconstruct_quicksort_tree(tree)
 end
 
 def build_quicksort_tree( value, tree = {} )
   return { value: value } if tree.nil?
 
-  if value < tree[:value]
-    tree[:left]  = build_quicksort_tree( value, tree[:left] )
-  else
-    tree[:right] = build_quicksort_tree( value, tree[:right] )
+  tree.tap do |tree|
+    if value < tree[:value]
+      tree[:left]  = build_quicksort_tree( value, tree[:left] )
+    else
+      tree[:right] = build_quicksort_tree( value, tree[:right] )
+    end
   end
-
-  tree
 end
 
 def deconstruct_quicksort_tree(tree)
-  array = []
-
-  array = array + deconstruct_quicksort_tree(tree[:left])  if tree[:left]
-  array = array + [tree[:value]]
-  array = array + deconstruct_quicksort_tree(tree[:right]) if tree[:right]
-
-  array
+  [].tap do |array|
+    array.concat(deconstruct_quicksort_tree(tree[:left]))  if tree[:left]
+    array.concat([tree[:value]])
+    array.concat(deconstruct_quicksort_tree(tree[:right])) if tree[:right]
+  end
 end
 
 if __FILE__ == $0
