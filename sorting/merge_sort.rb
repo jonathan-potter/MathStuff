5aaac2b77a7beef49b65283b4d46ffc2d6bfc019
Jonathan Potter <jonathan.potter.business@gmail.com>
2013-11-16 22:22:50 -0500

    refactors merge_sort for greater readability.

--- sorting/merge_sort.rb
+++ sorting/merge_sort.rb
@@ -1,25 +1,23 @@
 def merge_sort(array)
   return array if array.length <= 1
 
+  left, right = split_array(array)
+
+  left  = merge_sort(left)
+  right = merge_sort(right)
+
+  merge_arrays(left, right)
+end
+
+def split_array(array)
   half_index = array.length/2
+  [array[0...half_index], array[half_index..-1]]
+end
 
-  # sort the first and second half of the given array individually
-  first_half =  merge_sort(array[0...half_index])
-  second_half = merge_sort(array[half_index..-1])
-
-  # merge the two sorted arrays
-  sorted_array = []
-  while first_half.length > 0 and second_half.length > 0
-    if first_half[0] < second_half[0]
-      sorted_array << first_half.shift
-    else
-      sorted_array << second_half.shift
-    end
-  end
-
-  # add the remaining elements to the end of the array
-  sorted_array = sorted_array + first_half
-  sorted_array = sorted_array + second_half
+def merge_arrays(left, right)
+  return left.empty? ? right : left if left.empty? || right.empty?
+  smallest = left[0] < right[0] ? left.shift : right.shift
+  merge_arrays(left, right).unshift(smallest)
 end
 
 if __FILE__ == $0
--- tests/merge_sort_spec.rb
+++ tests/merge_sort_spec.rb
@@ -3,9 +3,21 @@ require_relative '../sorting/merge_sort.rb'
 require 'rspec'
 
 describe "Mergesort" do
+
   randomized_array = (1..100).to_a.shuffle
 
-  it "sorts an array" do
-    quicksort(randomized_array).should == randomized_array.sort
+  it "splits an array in half" do
+    split_array([1,2,3,4,5,6]).should == [[1,2,3], [4,5,6]]
+    split_array([1,2,3,4,5]).should   == [[1,2],   [3,4,5]]
   end
+
+  it "merges two sorted arrays into a sorted array" do
+    merge_arrays([1,3,5], [2,4,6]).should          == [1,2,3,4,5,6]
+    merge_arrays([1,2,3,4,5], [6,7,8,9,10]).should == [1,2,3,4,5,6,7,8,9,10]
+  end
+
+  it "sorts an array" do
+    merge_sort(randomized_array).should == randomized_array.sort
+  end
+
 end
